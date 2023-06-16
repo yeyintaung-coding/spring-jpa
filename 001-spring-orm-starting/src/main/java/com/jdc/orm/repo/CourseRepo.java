98f18ea43b8a57c0a2dedf6ffa8e5ce8e0211849
User <User@192.168.99.137>
2023-06-16 12:41:06 +0630

    First JPA

---
+++ 001-spring-orm-starting/src/main/java/com/jdc/orm/repo/CourseRepo.java
@@ -0,0 +1,25 @@
+package com.jdc.orm.repo;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jdc.orm.entity.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class CourseRepo {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Transactional
+	public Course create(Course course) {
+//		em.getTransaction().begin();		
+		em.persist(course);
+//		em.getTransaction().commit();
+		
+		return course;
+	}
+}
